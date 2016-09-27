aa4ffa3954017a9677003f35884e35d0d9defe2f
alankrita18.as@gmail.com <alankrita18.as@gmail.com>
2016-09-27 19:44:00 +0530

    i included tables

--- interface/admin.py
+++ interface/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from interface.models import *
 # Register your models here.
+
+admin.site.register(Player)
+admin.site.register(Match)
+admin.site.register(PlayerMatch)
+admin.site.register(Team)
+admin.site.register(PlayerTeam)
+admin.site.register(Ground)
+admin.site.register(Series)
--- interface/models.py
+++ interface/models.py
@@ -3,3 +3,54 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    PlayerID = models.CharField(max_length=20, primary_key=True)
+    PlayerName = models.CharField(max_length=100)
+    DOB = models.DateField()
+
+class Match(models.Model):
+    MatchID = models.IntegerField(primary_key=True)
+    TeamID1 = models.IntegerField()
+    TeamID2 = models.IntegerField()
+    SeriesID = models.IntegerField()
+    GroundID = models.IntegerField()
+    MDate = models.DateField()
+    Umpire = models.CharField(max_length=10)
+
+class PlayerMatch(models.Model):
+    PlayerID = models.ForeignKey(Player, on_delete=models.CASCADE)
+    MatchID = models.ForeignKey(Match, on_delete=models.CASCADE)
+    Fours = models.IntegerField()
+    Sixes = models.IntegerField()
+    TotalRuns = models.IntegerField()
+    Catches = models.IntegerField()
+    Wickets = models.IntegerField()
+    Overs = models.IntegerField()
+    Type = models.CharField(max_length=5)
+    Strategy = models.CharField(max_length=10)
+
+class Team(models.Model):
+    TeamID = models.IntegerField(primary_key=True)
+    HostGroundID = models.IntegerField()
+    Coach = models.CharField(max_length=20)
+
+class PlayerTeam(models.Model):
+    PlayerID = models.ForeignKey(Player, on_delete=models.CASCADE)
+    TeamID = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class Ground(models.Model):
+    GroundID = models.IntegerField(primary_key=True)
+    GroundType = models.CharField(max_length=20)
+    PitchType = models.CharField(max_length=20)
+
+class Series(models.Model):
+    SeriesID = models.IntegerField(primary_key=True)
+    GroundID = models.ForeignKey(Ground, on_delete=models.CASCADE)
+    ManOfTheSeries = models.IntegerField()
+
+
+
+
+
+
+ 
